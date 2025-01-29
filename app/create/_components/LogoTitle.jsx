856be8828bef18df30f9ed3ed6a095feca6b9cf7
Kan <kanalovefootball@gmail.com>
2025-01-29 11:09:26 +0700

    add Suspense to LogoTitle

--- app/create/_components/LogoTitle.jsx
+++ app/create/_components/LogoTitle.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import HeadingDescription from "./HeadingDescription";
 import Lookup from "@/app/_data/Lookup";
 import { useSearchParams } from "next/navigation";
@@ -10,20 +10,22 @@ function LogoTitle({ onHandleInputChange }) {
   const [title, setTitle] = useState(searchParam?.get("title") ?? "");
 
   return (
-    <div className="my-10">
-      <HeadingDescription
-        title={Lookup?.LogoTitle}
-        description={Lookup.LogoTitleDesc}
-      />
+    <Suspense>
+      <div className="my-10">
+        <HeadingDescription
+          title={Lookup?.LogoTitle}
+          description={Lookup.LogoTitleDesc}
+        />
 
-      <input
-        type="text"
-        placeholder={Lookup.InputTitlePlaceholder}
-        className="p-4 border rounded-lg mt-5 w-full"
-        defaultValue={title}
-        onChange={(e) => onHandleInputChange(e.target.value)}
-      />
-    </div>
+        <input
+          type="text"
+          placeholder={Lookup.InputTitlePlaceholder}
+          className="p-4 border rounded-lg mt-5 w-full"
+          defaultValue={title}
+          onChange={(e) => onHandleInputChange(e.target.value)}
+        />
+      </div>
+    </Suspense>
   );
 }
 
